6f5de10d4baebb26e87df897d95fc37977acd6cc
xizhiming <IORI@xizhimingdeMacBook-Pro.local>
2016-03-03 00:49:52 +0800

    f

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-var weixin      = require('weixin-api');  
-var express     = require('express');
-var superagent=require('superagent');
-var cheerio=require('cheerio');  
-var app         = express(); 
-var address="http://www.hdb.com";
+var weixin = require('weixin-api');
+var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var app = express();
+var address = "http://www.hdb.com";
 var items = [];
-function getItems(){
+
+function getItems() {
     superagent.get('http://www.hdb.com/timeline/lejz3')
-    .end(function(err, sres){
-        if(err){
-            return console.log(err);
-        }
-        var $=cheerio.load(sres.text);
-        $('#hd_lieb1 .find_main_li.img.canJoin').each(function (idx, element) {
-            var $element = $(element);
-            var timeStr=$element.find(".find_main_time p").text();
-            var time=timeStr.substring(0,10);
-            if(!time){
-                return;
+        .end(function(err, sres) {
+            if (err) {
+                return console.log(err);
             }
-            var title=$element.find('.find_main_title').text();
-            var href=$element.find("a[class=hd_pic_A]").attr('href');
+            var $ = cheerio.load(sres.text);
+            $('#hd_lieb1 .find_main_li.img.canJoin').each(function(idx, element) {
+                var $element = $(element);
+                var timeStr = $element.find(".find_main_time p").text();
+                var time = timeStr.substring(0, 10);
+                if (!time) {
+                    return;
+                }
+                var title = $element.find('.find_main_title').text();
+                var href = $element.find("a[class=hd_pic_A]").attr('href');
 
-            var imgSrc=$element.find("a img[class=hd_pic]").attr('src');
-            items.push({
-                title: title,
-                description:title,
-                picUrl:imgSrc,
-                time:time,
-                url: address+href
+                var imgSrc = $element.find("a img[class=hd_pic]").attr('src');
+                items.push({
+                    title: title,
+                    description: title,
+                    picUrl: imgSrc,
+                    time: time,
+                    url: address + href
+                });
             });
-        });
-        items.reverse();
+            items.reverse();
 
-    });
+        });
 }
 getItems();
 // 接入验证
@@ -51,105 +52,133 @@ app.get('/', function(req, res) {
 weixin.token = 'qbtest';
 
 // 监听文本消息
-weixin.textMsg(function(msg) {  
+weixin.textMsg(function(msg) {
 
     var resMsg = {};
 
     switch (msg.content) {
         case "更新":
-        console.log("更新");
-        getItems();
-        resMsg = {
-            fromUserName : msg.toUserName,
-            toUserName : msg.fromUserName,
-            msgType : "text",
-            content : "稍后回复‘活动’获取上海追梦户外最新活动列表！",
-            funcFlag : 0
-        }
-        weixin.sendMsg(resMsg);
-        break;
-        case "活动":
-        console.log('活动');
-        resMsg = {
-            fromUserName : msg.toUserName,
-            toUserName : msg.fromUserName,
-            msgType : "news",
-            articles : items.slice(0,5),
-            funcFlag : 0
-        }
-        weixin.sendMsg(resMsg);
-        break;
-        default:
-        superagent.get("http://www.tuling123.com/openapi/api?key=ce3555253d565d66b6c232ee8c587900&userid=jy02739244&info="+encodeURI(msg.content)).end(function(err,res){
-            console.log(res.text);
-            ;
+            console.log("更新");
+            getItems();
             resMsg = {
-                fromUserName : msg.toUserName,
-                toUserName : msg.fromUserName,
-                msgType : "text",
-                content : JSON.parse(res.text).text,
-                funcFlag : 0
-            };
-             weixin.sendMsg(resMsg)
-        });
-       
-        break;
-        // var articles = [];
-        // articles[0] = {
-        //     title : "漫步千年徽杭古道 挑战高山沙漠龙须山",
-        //     description : "漫步千年徽杭古道 挑战高山沙漠龙须山",
-        //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/61452348077740_party6.jpg",
-        //     url : "http://www.hdb.com/party/fw9x"
-        // };
-        // articles[1] = {
-        //     title : "徒步徽开古道 领略秘境白际",
-        //     description : "徒步徽开古道 领略秘境白际",
-        //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/61452348813669_party6.jpg",
-        //     url : "http://www.hdb.com/party/2s9x-PCHomeDetail.html"
-        // };
-        // articles[2] = {
-        //     title : "探访洞天福地观日出，游览八卦梯田赏云海",
-        //     description : "探访洞天福地观日出，游览八卦梯田赏云海",
-        //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/41452349110079_party4.jpg",
-        //     url : "http://www.hdb.com/party/ls9x-PCHomeDetail.html"
-        // };
-        
+                fromUserName: msg.toUserName,
+                toUserName: msg.fromUserName,
+                msgType: "text",
+                content: "稍后回复‘活动’获取上海追梦户外最新活动列表！",
+                funcFlag: 0
+            }
+            weixin.sendMsg(resMsg);
+            break;
+        case "活动":
+            console.log('活动');
+            resMsg = {
+                fromUserName: msg.toUserName,
+                toUserName: msg.fromUserName,
+                msgType: "news",
+                articles: items.slice(0, 5),
+                funcFlag: 0
+            }
+            weixin.sendMsg(resMsg);
+            break;
+        default:
+            var reg = /(^[1-9]|1[0-2])月活动$/;
+            var res = reg.match(msg.content);
+            if (res) {
+                var monthItems = [];
+                var monthReg = /^\d+-(0[1-9]|1[0-2])-\d{1,2}$/;
+                for (var i = 0; i < items.length; i++) {
+                    var item = items[i];
+                    var month = monthReg.match(item.time);
+                    if (month.length = 2 && month <= 10) {
+                        month = month.substring(1, 2);
+                    }
+                    if (res == month) {
+                        monthItems.push(items[i]);
+                    }
+                }
+                if (monthItems.length > 5) {
+                    monthItems = monthItems.slice(0, 5);
+                }
+                resMsg = {
+                    fromUserName: msg.toUserName,
+                    toUserName: msg.fromUserName,
+                    msgType: "news",
+                    articles: monthItems,
+                    funcFlag: 0
+                }
+                weixin.sendMsg(resMsg);
+            } else {
+                superagent.get("http://www.tuling123.com/openapi/api?key=ce3555253d565d66b6c232ee8c587900&userid=jy02739244&info=" + encodeURI(msg.content)).end(function(err, res) {
+                    console.log(res.text);;
+                    resMsg = {
+                        fromUserName: msg.toUserName,
+                        toUserName: msg.fromUserName,
+                        msgType: "text",
+                        content: JSON.parse(res.text).text,
+                        funcFlag: 0
+                    };
+                    weixin.sendMsg(resMsg);
+                });
+            }
+
+
+            break;
+            // var articles = [];
+            // articles[0] = {
+            //     title : "漫步千年徽杭古道 挑战高山沙漠龙须山",
+            //     description : "漫步千年徽杭古道 挑战高山沙漠龙须山",
+            //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/61452348077740_party6.jpg",
+            //     url : "http://www.hdb.com/party/fw9x"
+            // };
+            // articles[1] = {
+            //     title : "徒步徽开古道 领略秘境白际",
+            //     description : "徒步徽开古道 领略秘境白际",
+            //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/61452348813669_party6.jpg",
+            //     url : "http://www.hdb.com/party/2s9x-PCHomeDetail.html"
+            // };
+            // articles[2] = {
+            //     title : "探访洞天福地观日出，游览八卦梯田赏云海",
+            //     description : "探访洞天福地观日出，游览八卦梯田赏云海",
+            //     picUrl : "http://img2.hudongba.com/upload/_oss/userpartyimg/201601/09/41452349110079_party4.jpg",
+            //     url : "http://www.hdb.com/party/ls9x-PCHomeDetail.html"
+            // };
+
     }
 
-    
+
 });
 
 // 监听图片消息
-weixin.imageMsg(function(msg) {  
+weixin.imageMsg(function(msg) {
     console.log("imageMsg received");
     console.log(JSON.stringify(msg));
 });
 
 // 监听位置消息
-weixin.locationMsg(function(msg) {  
+weixin.locationMsg(function(msg) {
     console.log("locationMsg received");
     console.log(JSON.stringify(msg));
 });
 
 // 监听链接消息
-weixin.urlMsg(function(msg) {  
+weixin.urlMsg(function(msg) {
     console.log("urlMsg received");
     console.log(JSON.stringify(msg));
 });
 
 // 监听事件消息
-weixin.eventMsg(function(msg) {  
+weixin.eventMsg(function(msg) {
     console.log("eventMsg received");
     console.log(JSON.stringify(msg));
     console.log(msg.msgType);
     console.log(msg.event);
-    if(msg.msgType=='event'&&msg.event=='subscribe'){
+    if (msg.msgType == 'event' && msg.event == 'subscribe') {
         var resMsg = {
-            fromUserName : msg.toUserName,
-            toUserName : msg.fromUserName,
-            msgType : "text",
-            content : "欢迎关注，回复'活动'获取上海追梦户外最新活动！",
-            funcFlag : 0
+            fromUserName: msg.toUserName,
+            toUserName: msg.fromUserName,
+            msgType: "text",
+            content: "欢迎关注，回复'活动'获取上海追梦户外最新活动！",
+            funcFlag: 0
         };
         weixin.sendMsg(resMsg);
     }
@@ -164,4 +193,3 @@ app.post('/', function(req, res) {
 });
 
 app.listen(process.env.PORT || 5000);
-
